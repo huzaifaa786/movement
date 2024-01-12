d8b5a86d5fa507457f8798f5a03790dabf7eeec9
usama <tritechuzaifa@gmail.com>
2024-01-12 12:30:35 +0500

    company_add_payment_view

--- lib/views/company/add_payment/company_add_payment_controller.dart
+++ lib/views/company/add_payment/company_add_payment_controller.dart
@@ -1,5 +1,46 @@
 import 'package:get/get.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CompanyAddPaymentController extends GetxController {
   static CompanyAddPaymentController instance = Get.find();
+  RxInt selectedOption = 1.obs;
+
+  void setSelectedOption(int? value) {
+    selectedOption.value = value ?? 1;
+    update();
+  }
+
+   CalendarFormat format = CalendarFormat.month;
+  var format1 = 'month';
+  DateTime ourdate = DateTime.now();
+  DateTime today = DateTime.now();
+  DateTime? rangeStart;
+  DateTime? rangeEnd;
+
+  void onFormatChanged(DateTime date) {
+    today = date;
+    ourdate = date;
+
+    update();
+  }
+
+  void onDaySelected(DateTime day, DateTime foucsedDay) async {
+    today = day;
+    ourdate = day;
+  
+    update();
+  }
+
+  void onRangeSelected(start, end, focusedDay) {
+    rangeStart = start;
+    rangeEnd = end;
+    today = focusedDay;
+    ourdate = focusedDay;
+    if (end == null) {
+   
+    } else {
+    
+    }
+    update();
+  }
 }
