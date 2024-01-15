f4aaef566589d52319c0b37377053b439510ea51
usama <tritechuzaifa@gmail.com>
2024-01-15 16:06:35 +0500

    pull

--- lib/views/company/auth/signin/signin_controller.dart
+++ lib/views/company/auth/signin/signin_controller.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:noobz/api/loginapi.dart';
+import 'package:noobz/api/auth_api.dart';
 
 class CompanySignInController extends GetxController {
   static CompanySignInController instance = Get.find();
   TextEditingController passwordController = TextEditingController();
   TextEditingController emailController = TextEditingController();
 
-  final loginapi = LoginApi();
+  final authapi = UserApi();
 
   bool obscureTextPassword = true;
   void toggle() {
@@ -18,29 +18,29 @@ class CompanySignInController extends GetxController {
   String language = 'english';
 
   Future<void> companyloginUser() async {
-    try {
-      print('objectllllllllllllllllllllllllllllllllll');
+    // try {
+    //   print('objectllllllllllllllllllllllllllllllllll');
 
-      if (emailController.text.isEmpty || passwordController.text.isEmpty) {
-        return;
-      }
-      print('objectllllllllllllllllllllllllllllllllll');
-      var response = await loginapi.login(
-        emailController.text,
-        passwordController.text,
-      );
+    //   if (emailController.text.isEmpty || passwordController.text.isEmpty) {
+    //     return;
+    //   }
+    //   print('objectllllllllllllllllllllllllllllllllll');
+    //   var response = await authapi.login(
+    //     emailController.text,
+    //     passwordController.text,
+    //   );
 
-      if (response) {
-        print('Login successful');
-        // Add your logic here for what to do after a successful login
-      } else {
-        // Login failed
-        print('Login nottttttttttttt successful');
+    //   if (response) {
+    //     print('Login successful');
+    //     // Add your logic here for what to do after a successful login
+    //   } else {
+    //     // Login failed
+    //     print('Login nottttttttttttt successful');
 
-        // Add your logic here for what to do when login fails
-      }
-    } catch (error) {
-      print('Error logging in: $error');
-    }
+    //     // Add your logic here for what to do when login fails
+    //   }
+    // } catch (error) {
+    //   print('Error logging in: $error');
+    // }
   }
 }
--- lib/views/company/auth/signup/company_signup_controller.dart
+++ lib/views/company/auth/signup/company_signup_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:noobz/api/auth_api.dart';
 import 'package:noobz/utils/ui_utils.dart';
 
@@ -36,5 +37,32 @@ class CompanySignUPController extends GetxController {
     } catch (error) {
       print('Error registering user: $error');
     }
+  }
+
+  final picker = ImagePicker();
+  late XFile? selectedImage;
+
+  // Method to open the image picker and select an image
+  Future<void> pickImage() async {
+    try {
+      final pickedImage = await picker.pickImage(source: ImageSource.gallery);
+      if (pickedImage != null) {
+        selectedImage = pickedImage;
+        update();
+      }
+    } catch (error) {
+      print('Error picking image: $error');
+    }
+  }
+
+  // Method to upload the selected image
+  Future<void> uploadImage() async {
+    if (selectedImage == null) {
+      return;
+    }
+
+    // Implement your image uploading logic here
+    // You can use the selectedImage.path to access the image file path
+    // and upload it to your desired location.
   }
 }
