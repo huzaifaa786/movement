b3bf27dbb0fe44993ca30636f4be84ffdd5929b1
aliraza <tritechuzaifa@gmail.com>
2024-01-20 14:33:45 +0500

    ali

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -2,7 +2,6 @@ class User {
   late int id;
   String? username;
   String? email;
-  String? password;
   String? account_type;
   String? api_token;
   String? login_type;
@@ -11,7 +10,6 @@ class User {
     required this.id,
     this.username,
     this.email,
-    this.password,
     this.account_type,
     this.api_token,
     this.login_type,
@@ -22,7 +20,6 @@ class User {
       id: json['id'],
       username: json['name'],
       email: json['email'],
-      password: json['password'],
       account_type: json['account_type'],
       api_token: json['api_token'],
       login_type: json['login_type'],
@@ -34,7 +31,6 @@ class User {
       'name': username,
       'email': email,
       'api_token': api_token,
-      'password': password,
     };
   }
 }
